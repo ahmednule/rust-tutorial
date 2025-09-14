b35ccccabf1b9681d1bdae3b66b15c0e4aad7b99
ahmednule <nuleahmed6@gmail.com>
2025-09-14 18:35:32 +0300

    feature: hello world using cargo new command then use cargo run to execute

---
+++ hello_cargo/target/debug/deps/hello_cargo-755de162e2a3737c.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Desktop\rust-tutorial\hello_cargo\target\debug\deps\hello_cargo-755de162e2a3737c.d: src\main.rs
+
+C:\Users\Administrator\Desktop\rust-tutorial\hello_cargo\target\debug\deps\libhello_cargo-755de162e2a3737c.rmeta: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\Desktop\rust-tutorial\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Administrator\Desktop\rust-tutorial\hello_cargo\src\main.rs
